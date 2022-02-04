4c2d32dc7985f130b181c83dbb9e96e1b4ecfd90
JinseiWada <98995218+JinseiWada@users.noreply.github.com>
2022-02-04 11:27:08 +0900

    Add files via upload

---
+++ othello.py
@@ -0,0 +1,15 @@
+from turtle import getscreen
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption('othello')
+running = True
+while running:
+    screen.fill((0,255,0))
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            running = False
+
+    pygame.display.update()
